ec3a8a57a8cbfd4fd5cec6a0fdca01f3a4005cc1
Rohan Thomas <rohangeorgethomas@gmail.com>
2023-10-26 17:59:52 -0700

    Added xyz options to reposition script. Added Camera tutorial scene

---
+++ Assets/Scripts/Player/RepositionPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepositionPlayer : MonoBehaviour
+{
+    // Add this to the holographic object
+    [SerializeField] private GameObject _player;
+    [SerializeField] private bool repositionX = true;
+    [SerializeField] private bool repositionY = true;
+    [SerializeField] private bool repositionZ = true;
+
+    private Vector3 telePostion;
+    void Start()
+    {
+        telePostion = transform.GetChild(0).position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Reposition();
+        }
+    }
+    private void Reposition()
+    {
+        _player.GetComponent<CharacterController>().enabled = false;
+        _player.transform.position = new Vector3(repositionX ? telePostion.x : _player.transform.position.x,
+                                                 repositionY ? telePostion.y : _player.transform.position.y,
+                                                 repositionZ ? telePostion.z : _player.transform.position.z);
+        _player.GetComponent<CharacterController>().enabled = true;
+    }
+
+}
