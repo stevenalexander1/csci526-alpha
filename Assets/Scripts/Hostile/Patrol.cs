5b1422b36e416810d5a37ff4c135a9b8e1fb72dd
Srikar Kalivarapu <kalivara@usc.edu>
2023-10-28 19:11:05 -0700

    Guard Smooth Rotation script updated

--- Assets/Scripts/Hostile/Patrol.cs
+++ Assets/Scripts/Hostile/Patrol.cs
@@ -10,16 +10,20 @@ public class Patrol : MonoBehaviour
     private float startWaitTime;
     private float waitTime;
     public Transform locations;
-    
+
     private List<Transform> moveSpots = new List<Transform>();
     private int index = 1;
     private int increment = 1;
 
+    // Variables for smooth rotation
+    [SerializeField] private float rotationSpeed = 2.0f; // Adjust the rotation speed as needed
+    private Quaternion targetRotation;
+
     void Start()
     {
-        foreach(Transform child in locations)
+        foreach (Transform child in locations)
         {
-            moveSpots.Add(child); // Adding patrol locations to list
+            moveSpots.Add(child); // Adding patrol locations to the list
         }
         waitTime = startWaitTime;
         if (moveSpots.Count == 0)
@@ -27,23 +31,28 @@ public class Patrol : MonoBehaviour
         transform.LookAt(moveSpots[index]);
     }
 
-
     void Update()
     {
         if (moveSpots.Count == 0)
             return;
+
+        // Calculate the direction to the next patrol point
+        Vector3 targetDirection = moveSpots[index].position - transform.position;
+
+        // Smoothly interpolate the rotation to the new look direction
+        targetRotation = Quaternion.LookRotation(targetDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
         transform.position = Vector3.MoveTowards(transform.position, moveSpots[index].position, speed * Time.deltaTime);
 
-        if(Vector2.Distance(transform.position, moveSpots[index].position)<0.2f)
+        if (Vector2.Distance(transform.position, moveSpots[index].position) < 0.2f)
         {
             if (waitTime <= 0)
             {
                 if (index == 0 || index == moveSpots.Count - 1)
                     increment *= -1;
                 index += increment;
-                transform.LookAt(moveSpots[index]);
                 waitTime = startWaitTime;
-
             }
             else
                 waitTime -= Time.deltaTime;
